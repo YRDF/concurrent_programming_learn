af9b22f29a2e3571d184adfa7c4e2df43b114d1b
YRDF <zzh5653@163.com>
2024-10-07 16:23:56 +0800

    使用detach的隐患

--- first.cpp
+++ first.cpp
@@ -14,6 +14,27 @@ public:
     }
 };
 
+struct func {
+    int& _i;
+    func(int & i): _i(i){}
+    void operator()() {
+        for (int i = 0; i < 3; i++) {
+            _i = i;
+            std::cout << "_i is " << _i << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+};
+
+void oops() {
+
+        int some_local_state = 0;
+        func myfunc(some_local_state);
+        std::thread functhread(myfunc);
+        //隐患，访问局部变量，局部变量可能会随着}结束而回收或随着主线程退出而回收
+        functhread.detach();    
+}
+
 int main()
 {
     std::string hellostr = "hello world !";
@@ -41,8 +62,12 @@ int main()
     t4.join();
     //join就是主线程等着子线程执行完了，主线程才能继续往下执行。
 
-    //detach
-    
+    //detach 让子线程在后台运行，不会受主线程的影响 
+    //线程允许采用分离的方式在后台独自运行，C++ concurrent programing书中称其为守护线程。
+    // detach 注意事项
+    oops();
+    //防止主线程退出过快，需要停顿一下，让子线程跑起来detach
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
     return 0;
 }
