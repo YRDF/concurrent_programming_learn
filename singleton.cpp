44b9fe9b5a75a5a201d1f3d03a5c53b597ab55f0
YRDF <zzh5653@163.com>
2024-10-10 10:28:26 +0800

    懒汉式结合智能指针单例类学习

--- singleton.cpp
+++ singleton.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<thread>
 #include<mutex>
+#include<memory>
 
+//有危险的单例类
 class Single2{
 private:
     Single2(){}
@@ -15,7 +17,7 @@ public:
     }
 };
 
-//饿汉式单例类
+//饿汉式单例类（使用之前就初始化了）
 class SingleHungry
 {
 private:
@@ -35,11 +37,12 @@ private:
     static SingleHungry* single;
 };
 
-//饿汉式初始化
+//饿汉式初始化和测试
 SingleHungry* SingleHungry::single = SingleHungry::GetInst();
 void thread_func_s2(int i)
 {
     std::cout << "this is thread " << i << std::endl;
+    //std::cout<<"this id is "<<std::this_thread::get_id()<<std::endl;
     std::cout << "inst is " << SingleHungry::GetInst() << std::endl;
 }
 void test_single2hungry()
@@ -53,8 +56,85 @@ void test_single2hungry()
     }
 }
 
+//懒汉式单例类（使用时才初始化,需要加锁）
+//但是不知道是谁new的，所以怎么回收？有可能多重释放或者不知道哪个指针释放
+class SinglePointer
+{
+private:
+    SinglePointer(){}
+    SinglePointer(const SinglePointer&) = delete;
+    SinglePointer& operator = (const SinglePointer&) = delete;
+
+public:
+    static SinglePointer* GetInst()
+    {
+        if(single!= nullptr)
+        {
+            return single;
+        }
+        s_mutex.lock();
+        if(single!=nullptr)
+        {
+            s_mutex.unlock();
+            return single;
+        }
+        single = new SinglePointer();
+        s_mutex.unlock();
+        return single;        
+    }
+private:
+    static SinglePointer *single;
+    static std::mutex s_mutex;
+};
+
+//可以交给智能指针管理
+class SingleAuto{
+private:
+    SingleAuto(){}
+    SingleAuto(const SingleAuto&) = delete;
+    SingleAuto & operator=(const SingleAuto&) = delete;
+public:
+    ~SingleAuto(){
+        std::cout<<"single auto delete succes "<<std::endl;
+    }
+
+    static std::shared_ptr<SingleAuto> GetInst(){
+        if(single != nullptr)
+        {
+            return single;
+        }
+        s_mutex.lock();
+        if(single != nullptr)
+        {   
+            s_mutex.unlock();
+            return single;
+        }
+        single = std::shared_ptr<SingleAuto>(new SingleAuto);
+        s_mutex.unlock();
+        return single;
+    }
+
+private:
+    static std::shared_ptr<SingleAuto> single;
+    static std::mutex s_mutex;
+};
+
+//调用如下
+std::shared_ptr<SingleAuto> SingleAuto::single = nullptr;
+std::mutex SingleAuto::s_mutex;
+void test_singleauto()
+{
+    auto sp1 = SingleAuto::GetInst();
+    auto sp2 = SingleAuto::GetInst();
+    std::cout << "sp1  is  " << sp1 << std::endl;
+    std::cout << "sp2  is  " << sp2 << std::endl;
+    //此时存在隐患，可以手动删除裸指针，造成崩溃
+    // delete sp1.get();
+}
+
 int main()
 {
-    test_single2hungry();
+    //test_single2hungry();
+    test_singleauto();
     return 0;
 }
