bd89cf9bc56a18c5d706d8622aa216abee376425
YRDF <zzh5653@163.com>
2024-10-17 09:35:50 +0800

    学习内存次序

--- atomic.cpp
+++ atomic.cpp
@@ -47,8 +47,41 @@ void TestSpinLock() {
     t2.join();
 }
 
+//内存序
+std::atomic<bool> x, y;
+std::atomic<int> z;
+
+void write_x_then_y() {
+    x.store(true, std::memory_order_relaxed);  // 1
+    y.store(true, std::memory_order_relaxed);  // 2
+}
+
+void read_y_then_x() {
+    while (!y.load(std::memory_order_relaxed)) { // 3
+        std::cout << "y load false" << std::endl;
+    }
+
+    if (x.load(std::memory_order_relaxed)) { //4
+        ++z;
+    }
+
+}
+
+void TestOrderRelaxed() {
+    x=false;
+    y=false;
+    z=0;
+
+    std::thread t1(write_x_then_y);
+    std::thread t2(read_y_then_x);
+    t1.join();
+    t2.join();
+    assert(z.load() != 0); // 5
+}
+
 int main()
 {
-    TestSpinLock();
+    //TestSpinLock();
+    //TestOrderRelaxed();
     return 0;
 }
