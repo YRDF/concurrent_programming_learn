f9cecc9f205accbfdefd7f44dbacdac803d18d1c
yrdf <zzh5653@163.com>
2025-05-05 19:00:23 +0800

    实现crtp

--- new_file/CRTP_task_management_system/Command.h
+++ new_file/CRTP_task_management_system/Command.h
@@ -2,12 +2,17 @@
 #include<iostream>
 #include<memory>
 #pragma once
+
+class CommandBase{
+public:
+    virtual void execute(const std::string& args) = 0;
+};
 // CRTP
 template<typename Derived>
-class Command{
+class Command:public CommandBase{
 public:
-    void excute(const std::string& args){
-        (static_cast<Derived*>(this))->excuteImpl(args);
+    void execute(const std::string& args){
+        (static_cast<Derived*>(this))->executeImpl(args);
     }
 };
 
@@ -18,9 +23,9 @@ public:
 // 添加任务命令
 class AddCommand: public Command<AddCommand>{
 public:
-    AddCommand(TaskManager& manager):TaskManager(manager){}
-    void excuteImpl(const std::string& args){
-        //锟斤拷锟斤拷锟斤拷锟斤拷
+    AddCommand(TaskManager& manager):taskManager(manager){}
+    void executeImpl(const std::string& args){
+        //简单的参数解析：描述,优先级,截止日期
         size_t pos1 = args.find(',');
         size_t pos2 = args.find(',', pos1 + 1);
         if (pos1 == std::string::npos || pos2 == std::string::npos) {
@@ -30,9 +35,86 @@ public:
         std::string description = args.substr(0, pos1);
         int priority = std::stoi(args.substr(pos1 + 1, pos2 - pos1 - 1));
         std::string dueDate = args.substr(pos2 + 1);
-        TaskManager.addTask(description, priority, dueDate);
+        taskManager.addTask(description, priority, dueDate);
         std::cout << "任务添加成功。" << std::endl;
     }
 private:
-    TaskManager& TaskManager;
+    TaskManager& taskManager;
 };
+
+//删除
+class DeleteCommand: public Command<DeleteCommand>{
+public:
+    DeleteCommand(TaskManager& manager):taskManager(manager){}
+    void executeImpl(const std::string& args){
+        try{
+            size_t pos;
+            int id = std::stoi(args, &pos);
+            if(pos != args.length()){
+                std::cout << "参数格式错误。请使用: delete <ID>" << std::endl;
+                return;
+            }
+            taskManager.deleteTask(id);
+            std::cout << "任务删除成功。" << std::endl;
+        }catch(const std::invalid_argument& e){
+            Logger::getInstance().log("参数格式错误");
+            return;
+        }catch(const std::out_of_range& e){
+            Logger::getInstance().log("参数格式错误");
+            return;
+        }  
+    }
+private:
+    TaskManager& taskManager;
+};
+
+//列出所有的任务
+class ListCommand:public Command<ListCommand>{
+public:
+    ListCommand(TaskManager& manager):taskManager(manager){}
+    void executeImpl(const std::string& args){
+        try{
+            int sortOption = 0;
+            size_t pos;
+            sortOption = std::stoi(args,&pos);
+            if(pos != args.length()){
+                Logger::getInstance().log("参数格式错误！");
+                return;
+            }
+            std::cout<<"当前任务列表:"<<std::endl;
+            taskManager.listTasks(sortOption);
+        }catch(const std::invalid_argument& e){
+            Logger::getInstance().log("参数格式错误");
+            return;
+        }catch(const std::out_of_range& e){
+            Logger::getInstance().log("参数格式错误");
+            return;
+        }  
+    }
+private:
+    TaskManager& taskManager;
+};
+
+// 更新任务命令
+class UpdateCommand : public Command<UpdateCommand> {
+    public:
+        UpdateCommand(TaskManager& manager) : taskManager(manager) {}
+        void executeImpl(const std::string& args) {
+            // 参数格式: ID,描述,优先级,截止日期
+            size_t pos1 = args.find(',');
+            size_t pos2 = args.find(',', pos1 + 1);
+            size_t pos3 = args.find(',', pos2 + 1);
+            if (pos1 == std::string::npos || pos2 == std::string::npos || pos3 == std::string::npos) {
+                std::cout << "参数格式错误。请使用: update <ID>,<描述>,<优先级>,<截止日期>" << std::endl;
+                return;
+            }
+            int id = std::stoi(args.substr(0, pos1));
+            std::string description = args.substr(pos1 + 1, pos2 - pos1 - 1);
+            int priority = std::stoi(args.substr(pos2 + 1, pos3 - pos2 - 1));
+            std::string dueDate = args.substr(pos3 + 1);
+            taskManager.updateTask(id, description, priority, dueDate);
+            std::cout << "任务更新成功。" << std::endl;
+        }
+    private:
+        TaskManager& taskManager;
+    };
--- new_file/CRTP_task_management_system/crtp_use.cpp
+++ new_file/CRTP_task_management_system/crtp_use.cpp
@@ -2,13 +2,51 @@
 #include"Logger.h"
 #include"TaskManager.h"
 #include"Command.h"
+#include <unordered_map>
 
 int main(){
     // std::cout<<"hello world!"<<std::endl;
     // Logger::getInstance().log("hello world!");
-    TaskManager tm1;
-    AddCommand addc(tm1);
-    addc.excute("study,1,2025-05-05");
+    // TaskManager tm1;
+    // AddCommand addc(tm1);
+    // addc.excute("study,1,2025-05-05");
+    TaskManager taskManager;
+
+    std::unordered_map<std::string,std::unique_ptr<CommandBase>> commands;
+    commands["add"] = std::make_unique<AddCommand>(taskManager);
+    commands["delete"] = std::make_unique<DeleteCommand>(taskManager);
+    commands["list"] = std::make_unique<ListCommand>(taskManager);
+    commands["update"] = std::make_unique<UpdateCommand>(taskManager);
+
+    std::cout << "欢迎使用任务管理系统！" << std::endl;
+    std::cout << "可用命令: add, delete, list, update, exit" << std::endl;
+
+    std::string input;
+    while (true) {
+        std::cout << "\n> ";
+        std::getline(std::cin, input);
+        if (input.empty()) continue;
+
+        // 分离命令和参数
+        size_t spacePos = input.find(' ');
+        std::string cmd = input.substr(0, spacePos);
+        std::string args;
+        if (spacePos != std::string::npos) {
+            args = input.substr(spacePos + 1);
+        }
+
+        if (cmd == "exit") {
+            std::cout << "退出程序。" << std::endl;
+            break;
+        }
+
+        auto it = commands.find(cmd);
+        if (it != commands.end()) {
+            it->second->execute(args);
+        } else {
+            std::cout << "未知命令：" << cmd << std::endl;
+        }
+    }
 
     return 0;
 }
