7fa35d9fbb7d273c7251a87b23ea25198169d09b
yrdf <zzh5653@163.com>
2025-05-20 12:10:40 +0800

    singleton

--- new_file/future_promise_async/future_promise_async.cpp
+++ new_file/future_promise_async/future_promise_async.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
+
+class Single{
+public:
+    static Single& GetInstance(){
+        static Single s1;
+        return s1;
+    }
+private:
+    Single(){}
+    Single(const Single& s1) = delete;
+    Single& operator= (const Single& s1) = delete;
+};
+
+void testSingel(){
+    std::cout<<"s1 add is: "<<&Single::GetInstance()<<std::endl;
+    std::cout<<"s2 add is: "<<&Single::GetInstance()<<std::endl;
+}
+
 int main(){
     std::cout<<"hello world!"<<std::endl;
+    // Single s1;
+    // Single s2;
+    //testSingel();
     return 0;
 }
